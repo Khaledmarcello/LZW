999c03ea84b98b967513d1a86b75bde6af76be1f
Youssef Ahmed <youssef9300@hotmail.com>
2021-01-10 05:04:22 +0200

    Refractoring

--- src/LZW.java
+++ src/LZW.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class LZW {
-    public String ReadFromFile (String Path) throws IOException {
+    public String ReadFromFile(String Path) throws IOException {
         String content = null;
         File file = new File(Path);
         FileReader reader = null;
@@ -26,11 +26,12 @@ public class LZW {
         }
         return content;
     }
-    public void WriteToFile(String content,String name){
+
+    public void WriteToFile(String content, String name) {
         BufferedWriter writer = null;
-        String fileName = name+".txt" ;
+        String fileName = name + ".txt";
         File logFile = new File(fileName);
-        try{
+        try {
             writer = new BufferedWriter(new FileWriter(logFile));
             writer.write(content);
         } catch (Exception e) {
@@ -38,104 +39,102 @@ public class LZW {
         } finally {
             try {
                 writer.close();
-            } catch (Exception e) {}
+            } catch (Exception e) {
+            }
 
         }
     }
-    public int Exist(String chars, Vector<String> d){
-        int len = d.size() , num = chars.length();
-        if (num == 1)
-        {
+
+    public int Exist(String chars, Vector<String> d) {
+        int len = d.size(), num = chars.length();
+        if (num == 1) {
             char c = chars.toCharArray()[0];
-            return ((int)c) ;
+            return ((int) c);
         }
 
-        for (int i = 0; i < len ; i++)
-        {
+        for (int i = 0; i < len; i++) {
             if (chars.equals(d.get(i)))
-                return i+128;
+                return i + 128;
         }
         return -1;
     }
-    public void Compress(String path , String name) {
+
+    public void Compress(String path, String name) {
         String text = new String();
-        String compressed = new String ("") ;
-        Vector<String> d = new Vector<String>() ;
+        String compressed = new String("");
+        Vector<String> d = new Vector<String>();
 
         try {
             text = ReadFromFile(path);
-        } catch (IOException e) {}
+        } catch (IOException e) {
+        }
 
         int len = text.length();
         String end = "";
 
-        for (int i = 0; i<len; i++)
-        {
-            String curr =new String("");
-            curr += (text.toCharArray()[i]) ;
-            int ptr = 0 ;
-            while (Exist(curr, d)!=-1)
-            {
+        for (int i = 0; i < len; i++) {
+            String curr = new String("");
+            curr += (text.toCharArray()[i]);
+            int ptr = 0;
+            while (Exist(curr, d) != -1) {
                 i++;
-                ptr = Exist(curr , d) ;
+                ptr = Exist(curr, d);
 
-                if (i==len)
-                    break ;
+                if (i == len)
+                    break;
                 curr += text.toCharArray()[i];
 
             }
             d.addElement(curr);
-            end += curr ;
+            end += curr;
 
-            compressed+=ptr+" ";
+            compressed += ptr + " ";
             i--;
         }
 
-        WriteToFile(compressed , name);
+        WriteToFile(compressed, name);
 
     }
-    public void Decompress(String path , String name) {
+
+    public void Decompress(String path, String name) {
         String text = new String();
-        String decompressed = new String ("") ;
-        Vector<String> d = new Vector<String>() ;
+        String decompressed = new String("");
+        Vector<String> d = new Vector<String>();
         try {
             text = ReadFromFile(path);
-        } catch (IOException e) {}
+        } catch (IOException e) {
+        }
 
         int len = text.length();
-        Scanner stream = new Scanner (text) ;
-        String temp = "" ;
-        while (stream.hasNextInt())
-        {	String Decomp = "" ;
+        Scanner stream = new Scanner(text);
+        String temp = "";
+        while (stream.hasNextInt()) {
+            String Decomp = "";
             int curr = stream.nextInt();
-            boolean exist = false , unknown = true ;
-            if (curr<=127)
-            {
-                exist = true ;
-                String str = "" ;
-                str += (char)curr ;
-                Decomp=str ;
-            }
-            else
-            {
-                if (curr-128 < d.size())
-                    Decomp=d.get(curr-128) ;
+            boolean exist = false, unknown = true;
+            if (curr <= 127) {
+                exist = true;
+                String str = "";
+                str += (char) curr;
+                Decomp = str;
+            } else {
+                if (curr - 128 < d.size())
+                    Decomp = d.get(curr - 128);
 
-                else
-                {
+                else {
                     Decomp = (temp + temp.toCharArray()[0]);
                 }
             }
 
-            decompressed+=Decomp ;
+            decompressed += Decomp;
 
-            if ((temp+Decomp.toCharArray()[0]).length() > 1)
-                d.add(temp+Decomp.toCharArray()[0]);
+            if ((temp + Decomp.toCharArray()[0]).length() > 1)
+                d.add(temp + Decomp.toCharArray()[0]);
 
             temp = Decomp;
 
         }
-        WriteToFile(decompressed , name);
+        WriteToFile(decompressed, name);
     }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,7 +1,8 @@
 public class main {
-    public static void main(String args[]){
-        LZW lzw=new LZW();
-        lzw.Compress("C:\\Users\\RCSC\\Documents\\GitHub\\LZW\\test.txt", "compressed");
-        lzw.Decompress("C:\\Users\\RCSC\\Documents\\GitHub\\LZW\\compressed.txt", "decompressed");
+    public static void main(String args[]) {
+        LZW lzw = new LZW();
+        //Take Care of the Path of the project
+        lzw.Compress("D:\\Java Repos\\LZW\\test.txt", "compressed");
+        lzw.Decompress("D:\\Java Repos\\LZW\\compressed.txt", "decompressed");
     }
 }
